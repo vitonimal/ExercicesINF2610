3ec44fb0d2902dc03bceba032d07f737794b6902
Vittorio Passuello-Dussault <vittorio.passuello-dussault@polymtl.ca>
2019-05-18 15:34:25 -0400

    process tree

--- ressources/codes/ProcessTree/tree.c
+++ ressources/codes/ProcessTree/tree.c
@@ -3,18 +3,15 @@
 #include <unistd.h>
 #include <stdio.h>
 int main( ) {
-    printf("lol\n");
     pid_t p;
     int i, n=5;
     for (i=1; i<n; i++) {
         p = fork();
         if (p > 0)
             break;
-	sleep(1);
-        printf(" Processus %d  de père %d. \n",  getpid(), getppid());
-        //sleep(2);
     }
-    //sleep(2);
+    sleep(1);
+    printf(" Processus %d  de père %d. \n",  getpid(), getppid());
     return 0;
 }
 
