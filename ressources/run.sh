89c72719b4d0ba2165cf6e517994900c4eae0b02
Vittorio Passuello-Dussault <vittorio.passuello-dussault@polymtl.ca>
2019-05-19 10:48:02 -0400

    ./a.out as first parameter

--- ressources/run.sh
+++ ressources/run.sh
@@ -7,5 +7,7 @@
 
 docker build --no-cache -t inf2610-code-runner-image -f \
     .docker/Dockerfile-run . 2>&1 > /dev/null
-argv="$@"
+argv="./a.out "
+argv+="${@:2}"
+echo $argv
 docker run -e "DIR=$1" -e "ARGC=$#" -e "ARGV=$argv" inf2610-code-runner-image
