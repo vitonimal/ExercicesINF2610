fad940b70ccfe1afce16a0c0f5d9b3a1ea6c140a
Vittorio <vittorio-passduss@outlook.com>
2019-05-26 10:30:09 -0400

    updated env manip

--- ressources/codes/ProcessusEtThreads/EnvManip/varenv.c
+++ ressources/codes/ProcessusEtThreads/EnvManip/varenv.c
@@ -3,17 +3,21 @@
 #include <stdlib.h>
 // char *chaine1;
 int main(int argc, const char *argv[], const char *envp[]) {
+    
+    int cpid = fork();
     int k;
 
     printf("Paramètres:\n");
     for (k = 0; k < argc; k++)
         printf("%d: %s\n", k, argv[k]);
 
-    //printf("Variables d'environnement:\n");
-    //for (k = 0; envp[k] != NULL; k++)
-    //    printf("%d: %s\n", k, envp[k]);
+    printf("---------------------------------------------------\n");
+    printf("Variables d'environnement:\n");
+    printf("---------------------------------------------------\n");
+    for (k = 0; envp[k] != NULL; k++)
+        printf("%d: %s\n", k, envp[k]);
+    printf("---------------------------------------------------\n");
 
-    int cpid = fork();
     char *newenviron[] = { "TOKEN_NOCHILD" };
     printf( "No_CHild=%s\n", getenv("TOKEN_NOCHILD")); 
     setenv("TOKEN_NOCHILD","TRUE",1);
