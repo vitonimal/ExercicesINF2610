89bcf88b6bb5ca340a47133614b43130dc5e6200
Vittorio <vittorio-passduss@outlook.com>
2019-05-26 11:59:33 -0400

    simplified run

--- ressources/run.sh
+++ ressources/run.sh
@@ -9,4 +9,4 @@ docker build --no-cache -t inf2610-code-runner-image -f \
     .docker/Dockerfile-run . 2>&1 > /dev/null
 argv="$@"
 # argv[0]="./a.out" TODO
-docker run --privileged -u root -e "DIR=$1" -e "ARGC=$#" -e "ARGV=$argv" inf2610-code-runner-image
+docker run --privileged -e "DIR=$1" -e "ARGC=$#" -e "ARGV=$argv" inf2610-code-runner-image
