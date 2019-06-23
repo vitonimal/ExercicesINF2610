b773689f1b05c78e39909312dd529a47ae7bda72
Vittorio <vittorio-passduss@outlook.com>
2019-06-22 22:01:23 -0400

    indent

--- ressources/codes/MemVirt/SegMem/shmp.c
+++ ressources/codes/MemVirt/SegMem/shmp.c
@@ -14,13 +14,17 @@ int main( ) {
 	char   *nom   = "/inf2610shm";
 	int     fd = shm_open(nom, O_RDWR|O_CREAT, 0600 );
         
-	if(fd==-1) exit(1);
+	if(fd==-1) {
+		exit(1);
+	}
 	int cpid = fork();
 	if (cpid) {
 		printf ("Processus %d : a cree  ou ouvert un segment\n", getpid());
 		ftruncate(fd, taille);
 		char *ptr = (char *) mmap(NULL, taille, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-		if(ptr==NULL) exit(1);
+		if(ptr==NULL) {
+			exit(1);
+		}
 		close(fd);
 		printf("Processus %d : a attache le segment %s  a son espace d'adressage: %p\n", getpid(), nom, ptr);
 		char msg[100]; 
@@ -33,8 +37,8 @@ int main( ) {
 	} else {
 		fd = shm_open(nom, O_RDWR, 0644 );
 		if(fd==-1) {
-                printf("Failed to shm open\n");
-                exit(1);
+			printf("Failed to shm open\n");
+			exit(1);
         }
         printf ("Processus %d a ouvert le segment %s\n", getpid(), nom);
         struct stat info;
@@ -43,7 +47,9 @@ int main( ) {
         char* ptr = (char *) mmap(NULL, taille, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
         close(fd);
 		printf ("Processus %d a attache le segment %s a son espace d'adressage :%p \n", getpid(), nom,ptr);
-        if(ptr==NULL) exit(1);
+        if(ptr==NULL) {
+			exit(1);
+		}
 		sleep(1);
         printf("Processus %d lit du segment le message  %s\n", getpid(), ptr);
         munmap(ptr,taille);
