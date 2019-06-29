157cd7f3ccd302f2fd111141c0b95e2d9347a5ad
Vittorio <vittorio-passduss@outlook.com>
2019-06-29 15:31:19 -0400

    cleaning

--- ressources/codes/Synchronisation/SpinLock/threads1spinlock.c
+++ ressources/codes/Synchronisation/SpinLock/threads1spinlock.c
@@ -11,28 +11,38 @@ void* inc_dec(void * x)
 	for(i=0; i<100000; i++)
 	{	pthread_spin_lock(&lock);
 		glob = glob + pas ;
-	//	for(j=0; j<50000; j++);
-		pthread_spin_unlock(&lock);
 		for(j=0; j<50000; j++);
+		pthread_spin_unlock(&lock);
+		// for(j=0; j<50000; j++);
 	}
         printf("ici inc_dec(%d), glob = %d, thread_id = %lu, pid=%d\n", pas, glob, pthread_self(), getpid());
         pthread_exit(NULL);
 }
 
 int main( )
-{   	pthread_t tid1, tid2;
+{   	
+	pthread_t tid1, tid2;
   	int th1=1, th2=-1;
-        printf("ici main, glob = %d\n", glob);
+	
+	printf("ici main, glob = %d\n", glob);
+	
 	pthread_spin_init(&lock,PTHREAD_PROCESS_PRIVATE);
-        //creation de deux threads
-        printf("ici main: creation du thread inc_dec(%d) avec succes\n",  th1);
-	if ( pthread_create(&tid1, NULL, inc_dec, (void*) &th1 )!= 0)    return -1;
+    
+	//creation de deux threads
+	if ( pthread_create(&tid1, NULL, inc_dec, (void*) &th1 )!= 0)
+		return -1;
+	printf("ici main: creation du thread inc_dec(%d) avec succes\n",  th1);
+	if ( pthread_create(&tid2, NULL, inc_dec, (void*) &th2) != 0)
+		return -1;
 	printf("ici main: creation du thread inc_dec(%d) avec succes\n", th2);
-	if ( pthread_create(&tid2, NULL, inc_dec, (void*) &th2) != 0)   return -1;
-        pthread_join(tid1,NULL); // attendre la fin du thread tid1
-        pthread_join(tid2,NULL);
-        printf("ici main : fin des threads, glob = %d \n",glob);
+    
+	pthread_join(tid1,NULL); // attendre la fin du thread tid1
+    pthread_join(tid2,NULL);
+    
+	printf("ici main : fin des threads, glob = %d \n",glob);
+	
 	pthread_spin_destroy(&lock);
-        return 0;
+    
+	return 0;
 }
 
