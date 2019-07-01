a0e1fb8b012692df1914fb831efcf2fa92b67713
Vittorio <vittorio-passduss@outlook.com>
2019-07-01 19:32:04 -0400

    lecteur redacteur

--- ressources/codes/Synchronisation/LecteurRedacteur/lecteur_redacteur.c
+++ ressources/codes/Synchronisation/LecteurRedacteur/lecteur_redacteur.c
@@ -6,26 +6,19 @@
 
 sem_t mutex;
 sem_t redact;
-sem_t tour;
 int db = 42;
 int nbL = 0;
-
-int getRandomTo(int limit) {
-    return rand() % limit + 1;
-}
 
 // BOUCLES INFNIES POUR OBSERVATION
 
 void* lecteur(void* arg) {
     while (1) {
-        sem_wait(&tour);
         sem_wait(&mutex);
         if (nbL == 0) {
             sem_wait(&redact); // il va y avoir un lecteur dans la bdd! Il faut decrementer le semaphore de redaction
         }
         nbL++;
         sem_post(&mutex);
-        sem_post(&tour);
         //lecture de la bdd
         sleep(1);
         printf("lecteur db=%d\n", db);
@@ -37,42 +30,41 @@ void* lecteur(void* arg) {
             sem_post(&redact); // il n'y a plus de lecteurs! les ecrivains peuvent y aller!
         }
         sem_post(&mutex);
-        sleep(getRandomTo(3));
     }
     return NULL;
 }
 
 void* redacteur(void* arg) {
     while (1) {
-        sem_wait(&tour);
         sem_wait(&redact);
         // modifier les donnees de la bd bd
         db++;
         printf("redacteur bd=%d\n", db);
-        sleep(2);
-        sem_post(&tour);
+        sleep(1);
         sem_post(&redact);
-        sleep(getRandomTo(5));
     }
     return NULL;
 }
 
 int main() {
-    pthread_t tid1, tid2;
+    const u_int8_t NUMBER_OF_THREADS = 5;
+    pthread_t tids[5];
 
     sem_init(&mutex, 0, 1);
     sem_init(&redact, 0, 1);
-    sem_init(&tour, 0, 1);
 
-    pthread_create(&tid1, NULL, lecteur, NULL);
-    pthread_create(&tid2, NULL, redacteur, NULL);
+    pthread_create(&tids[0], NULL, lecteur, NULL);
+    pthread_create(&tids[1], NULL, lecteur, NULL);
+    pthread_create(&tids[2], NULL, lecteur, NULL);
+    pthread_create(&tids[3], NULL, redacteur, NULL);
+    pthread_create(&tids[4], NULL, redacteur, NULL);
 
-    pthread_join(tid1, NULL);
-    pthread_join(tid2, NULL);
+    for (int i = 0; i < NUMBER_OF_THREADS; ++i) {
+        pthread_join(tids[i], NULL);
+    }
 
     sem_destroy(&mutex);
     sem_destroy(&redact);
-    sem_destroy(&tour);
 
     return 0;
 }
