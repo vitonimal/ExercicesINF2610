48f5a87ad19b6e2b60c30d0bc6f4ad5e1dfd67fe
Vittorio Passuello-Dussault <vittorio.passuello-dussault@polymtl.ca>
2019-07-03 19:13:08 -0400

    adapted script for windows

--- ressources/run.sh
+++ ressources/run.sh
@@ -9,5 +9,8 @@ docker build --no-cache -t inf2610-code-runner-image -f \
     .docker/Dockerfile-run . 2>&1 > /dev/null
 argv="$@"
 # argv[0]="./a.out" TODO
-
-docker run -it --log-driver none --privileged -e "DIR=$1" -e "ARGC=$#" -e "ARGV=$argv" inf2610-code-runner-image
+if [[ "$OSTYPE" == "msys" ]]; then
+    winpty docker run -it --log-driver none --privileged -e "DIR=$1" -e "ARGC=$#" -e "ARGV=$argv" inf2610-code-runner-image
+else
+    docker run -it --log-driver none --privileged -e "DIR=$1" -e "ARGC=$#" -e "ARGV=$argv" inf2610-code-runner-image
+fi
