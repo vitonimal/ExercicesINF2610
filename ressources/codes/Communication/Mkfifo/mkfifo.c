b4bf27c6576440abd3edc93802e08e0b9d62a1b1
Vittorio Passuello-Dussault <vittorio-passduss@outlook.com>
2019-09-10 07:41:04 -0400

    modification exo mkfifo

--- ressources/codes/Communication/Mkfifo/mkfifo.c
+++ ressources/codes/Communication/Mkfifo/mkfifo.c
@@ -8,65 +8,28 @@
 #include <sys/wait.h>
 
 int main () {
-
-    const char* PIPE_NAME = "monTube";
-
-    // Creation du tube
-    mkfifo(PIPE_NAME, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-
-    const pid_t cpid = fork();
-
-    if (!cpid) { // fils
-        int fd;
-        char message[50];
-        pid_t mainPID = getpid();
-
-        sprintf(message, "bonjour du writer [%d]", mainPID);
-
-        // Ouverture du pipe en mode ecriture et creation si !existe
-        fd = open("monTube", O_WRONLY);
-        printf("Ici writer [%d]\n", mainPID);
-
-        if (fd == -1) {
-            printf("Tube non disponible\n");
-            exit(1);
+     mkfifo("monTube", 0660);
+     if (!fork()) { // fils
+        char m[50];
+        int i;
+        int fd = open("monTube", O_WRONLY);
+        printf("Ici writer [%d]\n", getpid());
+        // Ecriture dans le pipe
+        for(i=0; i<4;i++) {
+            sprintf(m, "mess%d de [%d]\n", i,getpid());
+            write(fd, m, strlen(m) + 1); 
         }
-
-        // Ecriture d'un message dans le pipe
-        write(fd, message, strlen(message) + 1); // +1 pour le retour a la ligne
-        printf("message ecrit\n");
         close(fd);
-        exit(0);
     }
     else { // parent
-        
-        int fd;
-        int readStatus;
-        char message[100];
-
-        // ouverture du pipe en mode lecture
-        fd = open("monTube", O_RDONLY);
-        printf("Ici reader [%d] : attente de l ecriture\n", getpid());
-
-        if (fd == -1) {
-            printf("Tube non disponible\n");
-            return 1;
+        char m;
+        int fd = open("monTube", O_RDONLY);
+        // lire du pipe 
+        while (read(fd, &m, 1) > 0) {
+            write(1,&m,1);
         }
-
-        wait(NULL);
-        printf("Ici reader [%d] : attente terminee\n", getpid());
-
-        while (read(fd, message, 50) > 0) {
-            printf("Message lu: %s\n", message);
-            close(fd);
-        }
-
-        exit(0);
-
+       close(fd);
+       wait(NULL);
     }
-
-    wait(NULL);
-
-    return 0;
-
+    exit(0);
 }
