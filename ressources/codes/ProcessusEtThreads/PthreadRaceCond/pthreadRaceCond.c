ca3ca61cfb46bfd5aa9eec988067637d0696691c
Vittorio Passuello-Dussault <vittorio-passduss@outlook.com>
2019-09-16 09:35:23 -0400

    ajout pthread race cond

---
+++ ressources/codes/ProcessusEtThreads/PthreadRaceCond/pthreadRaceCond.c
@@ -0,0 +1,23 @@
+// pthreadRaceCond.c
+#include <unistd.h> //pour sleep
+#include <stdio.h>  // pour printf
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+long a=0;
+void* thread_funct(void * p)
+{      
+	printf("thread exÃ©cutant thread_funct(%ld)\n", (long)p);
+    for(long i=0;i<(long)p;i++) a++;
+    pthread_exit(NULL);
+}
+int main( )
+{        pthread_t tid1, tid2;
+    if ( pthread_create(&tid1, NULL, thread_funct, (void*)100000) != 0)    return -1;
+    if ( pthread_create(&tid2, NULL, thread_funct, (void*)200000) != 0)   return -1;
+   pthread_join(tid1,NULL);
+    pthread_join(tid2,NULL);
+    printf("thread principal se termine avec a =%ld\n", a);
+    return 0;
+}
